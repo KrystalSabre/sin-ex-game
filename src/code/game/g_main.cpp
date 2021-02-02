402b5c5341e8ad50ecd07baeb139c237eef55fe8
KrystalSabre <56433192+KrystalSabre@users.noreply.github.com>
2021-02-02 14:09:23 -0600

    Add files via upload

--- src/code/game/g_main.cpp
+++ src/code/game/g_main.cpp
@@ -321,7 +321,7 @@ void G_InitGame(void)
    }
    else
    {
-      sv_fatrockets = gi.cvar("sv_fatrockets", "1", CVAR_SERVERINFO);
+      sv_fatrockets = gi.cvar("sv_fatrockets", "0", CVAR_SERVERINFO);
    }
 
    parentmode        = gi.cvar("parentmode", "0", CVAR_USERINFO|CVAR_SERVERINFO|CVAR_ARCHIVE);
