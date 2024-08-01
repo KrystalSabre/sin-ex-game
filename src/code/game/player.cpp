d1de94d4331b0a5924e7d2dab0802045ce2a4259
KrystalSabre <hlfoxwell32@gmail.com>
2024-07-31 23:27:51 -0500

    fov

--- src/code/game/player.cpp
+++ src/code/game/player.cpp
@@ -128,7 +128,7 @@ Event EV_Player_DropFlag("dropflag", EV_CONSOLE);
 #define CROUCH_SPEED			110.0f
 #define ACCELERATION			10.0f
 #define TAUNT_TIME			1.0f
-#define FOV_ADJUST(fov,videomode) (atan((videomode >= 11 ? 16 : 64) / ((videomode >= 11 ? 9 : 37) / tan((atan(3 / (4 / tan(fov / 360 * M_PI))) * 360 / M_PI) / 360 * M_PI))) * 360 / M_PI)
+#define FOV_ADJUST(fov,videomode) (atan(16 / ((videomode >= 11 ? 9 : 10) / tan((atan((videomode >= 11 ? 3 : 4) / ((videomode >= 11 ? 4 : 5) / tan(fov / 360 * M_PI))) * 360 / M_PI) / 360 * M_PI))) * 360 / M_PI)
 
 /*
 ==============================================================================
