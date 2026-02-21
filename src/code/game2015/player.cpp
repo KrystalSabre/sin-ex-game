4fbc0dc8eb5344351a3e88991fe96fe6c5d7d0c0
KrystalSabre <hlfoxwell32@gmail.com>
2026-02-20 21:43:32 -0600

    music duration

--- src/code/game2015/player.cpp
+++ src/code/game2015/player.cpp
@@ -7616,7 +7616,7 @@ void Player::ChangeMusic(const char * current, const char * fallback, qboolean f
             if(level.music_default_duration[current_mood_num] > 0 && (!duration || level.music_default_duration[current_mood_num] < duration))
                duration = level.music_default_duration[current_mood_num];
 
-            if(force || !music_duration || music_forced == 2 || !(client->ps.current_music_mood == current_mood_num && music_duration <= level.time + duration))
+            if(!music_duration || !(client->ps.current_music_mood == current_mood_num && level.music_default_duration[current_mood_num] > 0 && music_duration <= level.time + duration))
                music_duration = -duration;
          }
          else if(duration && game.maxclients == 1 && gi.cvar("s_music", "1", 0)->value && current_mood_num != music_current_mood)
